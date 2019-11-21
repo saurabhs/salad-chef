9bdbf5aecf6f2c914485345a3616343bdbf1514e
Saurabh S <saurabhs@mailbox.org>
2019-11-21 21:20:11 +0530

    Add Order managers

---
+++ Assets/Scripts/Components/OrderCreator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class OrderCreator : MonoBehaviour
+    {
+        ///<summary>///
+        ///stores the food order customer ordered
+        ///</summary>///
+        private string _order = string.Empty;
+
+        public string OrderPlaced { get => _order; }
+        
+        private void Start()
+        {
+            _order = CreateOrder();
+
+            var timerComp = GetComponent<Timer>();
+            if(timerComp == null)
+                throw new System.Exception("Timer Component required...");
+
+            timerComp.SetWatingTime(GetWaitingTime());
+        }
+
+        private string CreateOrder()
+        {
+            return "A|B|C";
+        }
+
+        private float GetWaitingTime()
+        {
+            if(_order == string.Empty) 
+                throw new System.Exception("Invalid order...");
+
+            return 10f;
+        }
+    }
+}
---
+++ Assets/Scripts/Components/OrderReceiver.cs
@@ -0,0 +1,24 @@
+﻿using SaladChef.Events;
+using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class OrderReceiver : MonoBehaviour
+    {
+        public OnOrderReceived onOrderReceived = null;
+
+        private void Start()
+        {
+            onOrderReceived = new OnOrderReceived();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var timerComp = GetComponent<Timer>();
+            if(timerComp == null)
+                throw new System.Exception("Timer component required...");
+
+            onOrderReceived.Invoke(timerComp.TimeLeft, timerComp.WaitingTIme);
+        }
+    }
+}
