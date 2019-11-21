ee3664b64d54ae5302bf9074f3ebc6a3b2c472db
Saurabh S <saurabhs@mailbox.org>
2019-11-21 21:08:38 +0530

    Add Unity events
    - Event for waiting time completion
    - Event for order received

---
+++ Assets/Scripts/Events/SaladChefEvents.cs
@@ -0,0 +1,5 @@
+﻿namespace SaladChef.Events
+{
+    public class OnWaitTimeOver : UnityEngine.Events.UnityEvent { }
+    public class OnOrderReceived : UnityEngine.Events.UnityEvent<float, float> { }
+}
