a9f38a2a159a7ff28190b116ebae275fa2c9cca1
Saurabh S <saurabhs@mailbox.org>
2019-11-22 18:58:37 +0530

    Add OrderCarrier

---
+++ Assets/Scripts/Components/OrderCarrier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SaladChef.Core
+{
+    /// Contains the data for food cooked by player 
+    /// and delivered to the customer
+    public class OrderCarrier : MonoBehaviour
+    {
+        public string GetOrder()
+        {
+            print("Getting Correct Order");
+            return "A|B|C";
+        }
+
+        public string GetWrongOrder()
+        {
+            print("Getting Wrong Order");
+            return "A|D|C";
+        }
+    }
+}
