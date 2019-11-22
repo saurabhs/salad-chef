e518b8be4af19dada576d3bff965ab24404db05e
Saurabh S <saurabhs@mailbox.org>
2019-11-22 18:32:06 +0530

    Add Move class

---
+++ Assets/Scripts/Components/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace SaladChef.Core
+{
+    /// <summary>
+    /// Handles movement based on input
+    /// </summary>
+    public class Move : MonoBehaviour
+    {
+        public Vector3 target = Vector3.zero;
+
+        private void Start()
+        {
+            var orderValidator = GetComponent<OrderValidity>();
+            if(orderValidator == null)
+                throw new System.Exception("Cannot find OrderValidaor...");
+        }
+
+        public void ActivateMove()
+        {
+            InvokeRepeating("MoveTo", 0, Time.deltaTime);
+        }
+
+        private void MoveTo()
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target, 1 * Time.deltaTime);
+        }
+    }
+}
