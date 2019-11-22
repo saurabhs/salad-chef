96d3e95436afb8478753d42ecfbd728e14ba8513
Saurabh S <saurabhs@mailbox.org>
2019-11-22 18:42:42 +0530

    Add events for orders

--- Assets/Scripts/Events/SaladChefEvents.cs
+++ Assets/Scripts/Events/SaladChefEvents.cs
@@ -1,5 +1,8 @@
 ﻿namespace SaladChef.Events
 {
     public class OnTimeOver : UnityEngine.Events.UnityEvent { }
-    public class OnOrderReceived : UnityEngine.Events.UnityEvent<float, float> { }
+    public class OnOrderPlaced : UnityEngine.Events.UnityEvent<string> { }
+    public class OnOrderReceived : UnityEngine.Events.UnityEvent<UnityEngine.GameObject, string, float, float> { }
+    public class OnWrongOrderReceived : UnityEngine.Events.UnityEvent { }
+    public class OnOrderValidated : UnityEngine.Events.UnityEvent<UnityEngine.GameObject, float, bool> { }
 }
