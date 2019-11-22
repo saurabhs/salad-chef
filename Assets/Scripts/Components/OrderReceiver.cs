7511d55b17244d64e7c7e49ed8a2147a8d1f54e2
Saurabh S <saurabhs@mailbox.org>
2019-11-22 18:51:52 +0530

    Add player to event invoke on receive
    - Add MockReceive() for test
    - Update execution order of OrderReceive class

--- Assets/Scripts/Components/OrderReceiver.cs
+++ Assets/Scripts/Components/OrderReceiver.cs
@@ -1,15 +1,32 @@
-﻿using SaladChef.Events;
+﻿using System.Collections;
+using SaladChef.Events;
 using UnityEngine;
 
 namespace SaladChef.Core
 {
     public class OrderReceiver : MonoBehaviour
     {
+        ///<summary>///
+        ///event when the player submits the order
+        ///</summary>///
         public OnOrderReceived onOrderReceived = null;
 
         private void Start()
         {
+            print("OrderReceiver:Start");
             onOrderReceived = new OnOrderReceived();
+
+            StartCoroutine(MockReceive());
+        }
+
+        private IEnumerator MockReceive()
+        {
+            print("OrderReceiver:MockReceive");
+
+            yield return new WaitForSeconds(5f);
+
+            var player1 = GameObject.Find("Player1");
+            onOrderReceived.Invoke(player1, player1.GetComponent<OrderCarrier>().GetOrder(), 25f, 60f);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -18,7 +35,8 @@ namespace SaladChef.Core
             if(timerComp == null)
                 throw new System.Exception("Timer component required...");
 
-            onOrderReceived.Invoke(timerComp.TimeLeft, timerComp.WaitingTIme);
+            var player = other.gameObject;
+            onOrderReceived.Invoke(player, player.GetComponent<OrderCarrier>().GetOrder(), timerComp.TimeLeft, timerComp.WaitingTIme);
         }
     }
 }
