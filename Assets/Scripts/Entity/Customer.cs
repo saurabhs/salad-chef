ace58f419657fcd1e30e900f4bead73d0784a2a8
Saurabh S <saurabhs@mailbox.org>
2019-11-23 02:19:50 +0530

    Add randomized colour tint to customers

---
+++ Assets/Scripts/Entity/Customer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SaladChef.Entity
+{
+    public class Customer : MonoBehaviour
+    {
+        [SerializeField] private string _name = "Customer";
+
+        public string Name => _name;
+
+        private void Start()
+        {
+            //set random colour
+            GetComponent<SpriteRenderer>().color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
