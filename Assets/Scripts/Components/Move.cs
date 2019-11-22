39388aa9c752b77f562b90551c36dd88c8859e84
Saurabh S <saurabhs@mailbox.org>
2019-11-23 01:05:19 +0530

    Fix MoveTo move completion
    - Add MoveOver event

--- Assets/Scripts/Components/Move.cs
+++ Assets/Scripts/Components/Move.cs
@@ -1,37 +1,53 @@
 ﻿using System;
+using SaladChef.Events;
+using TMPro;
 using UnityEngine;
 
 namespace SaladChef.Core
 {
     /// <summary>
-    /// Handles movement based on input
+    /// Handles movement
     /// </summary>
     public class Move : MonoBehaviour
     {
+        /// <summary>
+        /// Handles movement based on input
+        /// </summary>
         public Vector3 target = Vector3.zero;
+
+        /// <summary>
+        /// Handles movement based on input
+        /// </summary>
+        [SerializeField] private float _speed = 1f;
+
+        /// <summary>
+        /// event to fire when the parent has complement movement
+        /// </summary>
+        public OnMoveComplete onMoveComplete = null;
 
         private void Start()
         {
-            var orderValidator = GetComponent<OrderValidator>();
-            if (orderValidator == null)
-                throw new System.Exception("Cannot find OrderValidaor...");
+            onMoveComplete = new OnMoveComplete();
         }
 
         public void ActivateMove()
         {
+            print("Move::ActivateMove");
             InvokeRepeating("MoveTo", 0, Time.deltaTime);
         }
 
         private void MoveTo()
         {
-            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target, 1 * Time.deltaTime);
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target, _speed * Time.deltaTime);
+            var position = transform.position;
 
-            if (Math.Floor(gameObject.transform.position.x) == Math.Floor(target.x) &&
-            Math.Floor(gameObject.transform.position.y) == Math.Floor(target.y) &&
-            Math.Floor(gameObject.transform.position.z) == Math.Floor(target.z))
+            if (Math.Round(position.x, MidpointRounding.AwayFromZero) == Math.Round(target.x, MidpointRounding.AwayFromZero) &&
+            Math.Round(position.y, MidpointRounding.AwayFromZero) == Math.Round(target.y, MidpointRounding.AwayFromZero) &&
+            Math.Round(position.z, MidpointRounding.AwayFromZero) == Math.Round(target.z, MidpointRounding.AwayFromZero))
             {
-                CancelInvoke("MoveTo");
                 print("Canceliing MoveTo invoke");
+                CancelInvoke("MoveTo");
+                onMoveComplete.Invoke();
             }
         }
     }
--- Assets/Scripts/Events/SaladChefEvents.cs
+++ Assets/Scripts/Events/SaladChefEvents.cs
@@ -1,8 +1,10 @@
 ﻿namespace SaladChef.Events
 {
     public class OnTimeOver : UnityEngine.Events.UnityEvent { }
+    public class OnCustomerSeated : UnityEngine.Events.UnityEvent { }
     public class OnOrderPlaced : UnityEngine.Events.UnityEvent<string> { }
     public class OnOrderReceived : UnityEngine.Events.UnityEvent<UnityEngine.GameObject, string, float, float> { }
     public class OnWrongOrderReceived : UnityEngine.Events.UnityEvent { }
     public class OnOrderValidated : UnityEngine.Events.UnityEvent<UnityEngine.GameObject, float, bool> { }
+    public class OnMoveComplete : UnityEngine.Events.UnityEvent { }
 }
