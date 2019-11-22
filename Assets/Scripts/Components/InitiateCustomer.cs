b2828b15bf89a4a70e67fac3caf1dda829854ed3
Saurabh S <saurabhs@mailbox.org>
2019-11-23 01:07:03 +0530

    Add Initial Customer movement / init

---
+++ Assets/Scripts/Components/InitiateCustomer.cs
@@ -0,0 +1,31 @@
+﻿using SaladChef.Events;
+using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class InitiateCustomer : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _target = Vector3.zero;
+
+        public OnCustomerSeated onCustomerSeated = null;
+
+        private void Start()
+        {
+            onCustomerSeated = new OnCustomerSeated();
+
+            //move in
+            var move = GetComponent<Move>();
+            if (move == null)
+                throw new System.Exception("Canot find Move component...");
+
+            move.onMoveComplete.AddListener(OnMoveComplete);
+            move.target = _target;
+            move.ActivateMove();
+        }
+
+        private void OnMoveComplete()
+        {
+            onCustomerSeated.Invoke();
+        }
+    }
+}
