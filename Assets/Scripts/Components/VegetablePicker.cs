ebea9b39b72cf59d9f20361dcbb4b5ce34505547
Saurabh S <saurabhs@mailbox.org>
2019-11-23 19:32:01 +0530

    Add vegetable picker

---
+++ Assets/Scripts/Components/VegetablePicker.cs
@@ -0,0 +1,25 @@
+﻿using SaladChef.Events;
+using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class VegetablePicker : MonoBehaviour
+    {
+        public OnVegetablePicked onVegetablePicked = null;
+
+        private void Start()
+        {
+            print("VegetablePicker::Start");
+            onVegetablePicked = new OnVegetablePicked(); 
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.tag.Equals("Vegetable"))
+            {
+                print("Veg name " + other.name);
+                onVegetablePicked.Invoke(other.gameObject);
+            }
+        }
+    }
+}
