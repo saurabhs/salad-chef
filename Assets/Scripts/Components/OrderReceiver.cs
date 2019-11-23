a5d95224829f17423784668e101966fa71b2f448
Saurabh S <saurabhs@mailbox.org>
2019-11-24 03:34:24 +0530

    Move Collision check betwen Player and Customer to ServeOrder
    - Remove MockReceive()

--- Assets/Scripts/Components/OrderReceiver.cs
+++ Assets/Scripts/Components/OrderReceiver.cs
@@ -14,28 +14,17 @@ namespace SaladChef.Core
         private void Start()
         {
             onOrderReceived = new OnOrderReceived();
-
-            StartCoroutine(MockReceive());
         }
 
-        private IEnumerator MockReceive()
+        public void Receive(GameObject player)
         {
-            yield return new WaitForSecondsRealtime(10f);
-            
-            print("OrderReceiver:MockReceive");
+            print("OrderReceiver:Receive");
 
-            var player1 = GameObject.Find("Player1");
-            onOrderReceived.Invoke(player1, player1.GetComponent<OrderCarrier>().GetWrongOrder(), 25f, 60f);
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
             var timerComp = GetComponent<Timer>();
             if(timerComp == null)
                 throw new System.Exception("Timer component required...");
 
-            var player = other.gameObject;
-            onOrderReceived.Invoke(player, player.GetComponent<OrderCarrier>().GetOrder(), timerComp.TimeLeft, timerComp.WaitingTIme);
+            onOrderReceived.Invoke(player, player.GetComponent<ServeOrder>().Order, timerComp.TimeLeft, timerComp.WaitingTIme);
         }
     }
 }
