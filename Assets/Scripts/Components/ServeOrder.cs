8fca1537defcc75426f75a9c49a86b4d6f1777e5
Saurabh S <saurabhs@mailbox.org>
2019-11-24 03:39:21 +0530

    Update ServeOrder to serve to customer
    - Refactor salad to _salad

--- Assets/Scripts/Components/ServeOrder.cs
+++ Assets/Scripts/Components/ServeOrder.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace SaladChef.Core
 {
     public class ServeOrder : MonoBehaviour
     {
-        [SerializeField] private List<Vegetable> salad = new List<Vegetable>();
+        /*[SerializeField]*/ private List<Vegetable> _salad = new List<Vegetable>();
+        public string Order {
+            get {
+                var sb = new StringBuilder();
+                _salad.ForEach(obj => sb.Append($"{obj.Name}|"));
+                return sb.ToString();
+            }
+        }
 
         private void Start()
         {
@@ -18,33 +25,43 @@ namespace SaladChef.Core
             var input = GetComponent<Input>();
             if (input == null)
                 throw new System.Exception("Cannot find Input component...");
+
             input.discard.AddListener(DiscardChoppedVegetable);
             input.serveSalad.AddListener(ServeSalad);
         }
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.tag.Equals("Customer"))
+            {
+                var customer = other.GetComponent<OrderReceiver>();
+                customer.Receive(gameObject);
+            }
+        }
+
         private void ServeSalad(int index)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
         private void MoveChoppedVegetableToPlate(Vegetable veggie)
         {
             print($"Adding chopped {veggie.name} to plate");
-            salad.Add(veggie);
+            _salad.Add(veggie);
         }
 
         private void DiscardChoppedVegetable()
         {
             //cannot discard an empty plate
-            if(salad.Count == 0)
+            if (_salad.Count == 0)
                 return;
 
-            salad.Clear();
+            _salad.Clear();
 
             var hud = GetComponent<HUD>();
             if (hud == null)
                 throw new System.Exception("Cannot find HD compnent...");
-            
+
             //TODO: add proper scoring
             hud.UpdateScore(-50, false);
         }
