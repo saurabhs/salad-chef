a35c8eed73ce1c5979dda7760f4049b803423df3
Saurabh S <saurabhs@mailbox.org>
2019-11-23 19:31:33 +0530

    Add Vegetable class

---
+++ Assets/Scripts/Components/Vegetable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class Vegetable : MonoBehaviour
+    {
+        [SerializeField] private string _name = "Vegetable";
+        public string Name => _name;
+        
+        [SerializeField] private float chopTime = 3f;
+
+        public float ChopTime => chopTime;
+    }
+}
