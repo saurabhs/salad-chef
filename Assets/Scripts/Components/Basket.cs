09b2956c91e59a8314ae7c3bc63be26aaf48f90d
Saurabh S <saurabhs@mailbox.org>
2019-11-23 19:30:35 +0530

    Add Basket to collect vegetables from table

---
+++ Assets/Scripts/Components/Basket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class Basket : MonoBehaviour
+    {
+        [SerializeField] private Stack<Vegetable> _picked = new Stack<Vegetable>();
+        public Stack<Vegetable> Picked => _picked;
+
+        private void Start()
+        {
+            var vegPicker = GetComponent<VegetablePicker>();
+            if(vegPicker == null)
+                throw new System.Exception("Cannot find VegetablePicker...");
+            vegPicker.onVegetablePicked.AddListener(OnPicked);
+        }
+
+        private void OnPicked(GameObject veggie)
+        {
+            var vegetable = veggie.GetComponent<Vegetable>();
+            if(vegetable != null && _picked.Count < 2 && !_picked.Contains(vegetable))
+            {
+                _picked.Push(vegetable);
+            }
+        }
+    }
+}
