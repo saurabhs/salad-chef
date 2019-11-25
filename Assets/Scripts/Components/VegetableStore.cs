8f30d85b543a4229ccf458d9facf8daa5d5e4f3c
Saurabh S <saurabhs@mailbox.org>
2019-11-25 17:57:00 +0530

    Add VegetableStore, a centralized ref to all vegetables

---
+++ Assets/Scripts/Components/VegetableStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class VegetableStore : MonoBehaviour
+    {
+        [SerializeField] private List<Vegetable> _store = null;
+        public List<Vegetable> Store => _store;
+    }
+}
