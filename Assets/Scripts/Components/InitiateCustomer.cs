2e8eec97eb63966ee4ee48adae5a750d8388993d
Saurabh S <saurabhs@mailbox.org>
2019-11-25 16:52:56 +0530

    Update Customer movement to reenter once their time is up
    - Refactor: Change Start to OnEnable

--- Assets/Scripts/Components/InitiateCustomer.cs
+++ Assets/Scripts/Components/InitiateCustomer.cs
@@ -5,27 +5,53 @@ namespace SaladChef.Core
 {
     public class InitiateCustomer : MonoBehaviour
     {
+        /// <summary>
+        /// sitting position of the customer in the salad bar
+        /// </summary>
         [SerializeField] private Vector3 _target = Vector3.zero;
+
+        /// <summary>
+        /// max time the customer to wait 
+        /// before changing to new and renter
+        //// the salad bar
+        /// </summary>
+        [SerializeField] private float _maxDelayBetweenReEntry = 3f;
 
         public OnCustomerSeated onCustomerSeated = null;
 
-        private void Start()
+        private void OnEnable()
         {
             onCustomerSeated = new OnCustomerSeated();
 
             //move in
             var move = GetComponent<Move>();
             if (move == null)
-                throw new System.Exception("Canot find Move component...");
+                throw new System.Exception("Cannot find Move component...");
 
             move.onMoveComplete.AddListener(OnMoveComplete);
-            move.target = _target;
-            move.ActivateMove();
+            move.target = new Vector3(transform.position.x, _target.y, transform.position.z);
+            move.ActivateMove(Enums.EState.Sitting);
+        }
+
+        private void OnDisable()
+        {
+            onCustomerSeated = null;
         }
 
         private void OnMoveComplete()
         {
-            onCustomerSeated.Invoke();
+            if (GetComponent<State>().CurrentState == Enums.EState.Sitting)
+                onCustomerSeated.Invoke();
+            else if (GetComponent<State>().CurrentState == Enums.EState.Exit)
+            {
+                gameObject.SetActive(false);
+                Invoke("Reactivate", Random.value * _maxDelayBetweenReEntry);
+            }
+        }
+
+        private void Reactivate()
+        {
+            gameObject.SetActive(true);
         }
     }
 }
