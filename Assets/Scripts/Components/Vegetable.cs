892cc48b502637eaf1819648b9ea63611612bc0e
Saurabh S <saurabhs@mailbox.org>
2019-11-25 19:48:21 +0530

    Remove setting the vegetable names at runtime

--- Assets/Scripts/Components/Vegetable.cs
+++ Assets/Scripts/Components/Vegetable.cs
@@ -4,13 +4,17 @@ namespace SaladChef.Core
 {
     public class Vegetable : MonoBehaviour
     {
+        ///<summary>
+        /// name of the vegetable
+        ///</summary>
         [SerializeField] private string _name = "Vegetable";
-        public string Name => _name;
-        
+
+        ///<summary>
+        /// time taken to chop the vegetable
+        ///</summary>
         [SerializeField] private float chopTime = 3f;
 
+        public string Name => _name;
         public float ChopTime => chopTime;
-
-        private void OnEnable() =>_name = gameObject.name;
     }
 }
