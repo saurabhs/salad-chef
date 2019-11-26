6049b82a0ba419ea630de42ca00cd7ce0d297757
Saurabh S <saurabhs@mailbox.org>
2019-11-26 16:46:28 +0530

    Add Gameover scene

---
+++ Assets/Scripts/Global/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SaladChef.Globals
+{
+    public class GameOver : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI player1;
+        [SerializeField] private TextMeshProUGUI player2;
+        [SerializeField] private Button restart;
+        
+        private void OnEnable()
+        {
+            player1.text = $"PLAYER1 {PlayerPrefs.GetInt("Player1Score")}";
+            player2.text = $"{PlayerPrefs.GetInt("Player2Score")} PLAYER2";
+
+            restart.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene("Main_old"));
+        }
+
+        private void OnDisable()
+        {
+            restart.onClick.RemoveAllListeners();
+        }
+    }
+}
---
+++ Assets/Scripts/Global/LevelController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using SaladChef.Core;
+using UnityEngine;
+
+namespace SaladChef.Globals
+{
+    public class LevelController : MonoBehaviour
+    {
+        /// <summary>
+        /// list of players level controller will track for game over
+        /// </summary>
+        [SerializeField] private List<GameObject> _players = new List<GameObject>();
+
+        [SerializeField] private GameObject _levelOverUI = null;
+
+        int count = 0;
+
+        private void OnEnable()
+        {
+            foreach(var player in _players)
+            {
+                player.GetComponent<Timer>().onTimeOver.AddListener(OnTimeOver);
+            }
+        }
+
+        private void OnTimeOver()
+        {
+            count++;
+            if(count == _players.Count)
+            {
+                Invoke("InitiateGameOver", 1f);
+            }
+        }
+
+        private void InitiateGameOver()
+        {
+            Time.timeScale = 0;
+            _levelOverUI.SetActive(false);
+        }
+    }
+}
