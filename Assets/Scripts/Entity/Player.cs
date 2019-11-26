5e17b5e14996b2da680076edb0942d00d7937eac
Saurabh S <saurabhs@mailbox.org>
2019-11-26 16:06:43 +0530

    Add player component to player prefab for initiating timer

---
+++ Assets/Scripts/Entity/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SaladChef.Core
+{
+    public class Player : MonoBehaviour
+    {
+        /// <summary>
+        /// time for which the game will lasts
+        /// </summary>
+        [SerializeField] private float _time = 300f;
+
+        private void OnEnable()
+        {
+            var timer = GetComponent<Timer>();
+            if(timer == null)    
+                throw new System.Exception("Cannot find Timer...");
+            timer.SetWatingTime(_time);
+        }
+    }
+}
